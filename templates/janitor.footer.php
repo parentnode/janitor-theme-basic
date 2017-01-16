7073ea30785389ee262fb0b3c7ca4ab3bce85fc0
Martin Kæstel Nielsen <martin@think.dk>
2017-01-16 16:21:14 +0100

    Updated demo item

--- templates/janitor.footer.php
+++ templates/janitor.footer.php
@@ -1,14 +1,31 @@
 	</div>
 
 	<div id="navigation">
-		<ul>
-			<?= $HTML->link("Posts", "/janitor/post/list", array("wrapper" => "li.post")) ?>
-
-			<?= $HTML->link("Navigations", "/janitor/admin/navigation/list", array("wrapper" => "li.navigation")) ?>
-			<?= $HTML->link("Users", "/janitor/admin/user/list", array("wrapper" => "li.user")) ?>
-			<?= $HTML->link("Tags", "/janitor/admin/tag/list", array("wrapper" => "li.tags")) ?>
-
-			<?= $HTML->link("Profile", "/janitor/admin/profile", array("wrapper" => "li.profile")) ?>
+		<ul class="navigation">
+			<li class="content">
+				<h3>Content</h3>
+				<ul class="subjects">
+					<?= $HTML->link("Posts", "/janitor/admin/page/list", array("wrapper" => "li.post")) ?>
+					<?= $HTML->link("Demo items", "/janitor/demo/list", array("wrapper" => "li.demo")) ?>
+				</ul>
+			</li>
+			<li class="site">
+				<h3>Site</h3>
+				<ul class="subjects">
+					<?= $HTML->link("Navigations", "/janitor/admin/navigation/list", array("wrapper" => "li.navigation")) ?>
+					<?= $HTML->link("Tags", "/janitor/admin/tag/list", array("wrapper" => "li.tags")) ?>
+					<?= $HTML->link("Log", "/janitor/admin/log/list", array("wrapper" => "li.logs")) ?>
+					<?= $HTML->link("Cache", "/janitor/admin/system/cache", array("wrapper" => "li.cache")) ?>
+				</ul>
+			</li>
+			<li class="users">
+				<h3>Users</h3>
+				<ul class="subjects">
+					<?= $HTML->link("Users", "/janitor/admin/user/list", array("wrapper" => "li.user")) ?>
+					<?= $HTML->link("Groups", "/janitor/admin/user/group/list", array("wrapper" => "li.usergroup")) ?>
+					<?= $HTML->link("Profile", "/janitor/admin/profile", array("wrapper" => "li.profile")) ?>
+				</ul>
+			</li>
 		</ul>
 	</div>
 
