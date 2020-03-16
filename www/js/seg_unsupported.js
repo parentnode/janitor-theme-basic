b1e35b6b2646d3472b9cb386ee034dee84bcf2ba
Peter Kai Møller Pedersen <36038081+Threepwoodd@users.noreply.github.com>
2020-03-16 17:03:32 +0100

    Build

--- www/js/seg_unsupported.js
+++ www/js/seg_unsupported.js
@@ -1,7 +1,10 @@
+/*
+asset-builder @ 2020-03-16 17:03:21
+*/
 
 /*seg_unsupported_include.js*/
 
-/*i-unsupported.js*/
+/*m-unsupported.js*/
 function unsupported() {
 	document.body.innerHTML = '<h1 style="text-align: center; margin: 20% 15% 15%; font-family: Arial; color: #333333">Your browser is NOT supported. It is more outdated than steam-engines, typewriters and VHS tapes, so stop acting surprised.</h1>';
 }
