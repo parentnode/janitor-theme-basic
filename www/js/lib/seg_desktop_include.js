939f1985f7a371a0604b0cb0721793835fc77095
Peter Kai Møller Pedersen <36038081+Threepwoodd@users.noreply.github.com>
2020-03-16 16:59:58 +0100

    Update file names and includes

--- www/js/lib/seg_desktop_include.js
+++ www/js/lib/seg_desktop_include.js
@@ -3,8 +3,8 @@ document.write('<script type="text/javascript" src="/js/manipulator/v0_9_2/merge
 document.write('<script type="text/javascript" src="/js/lib/u-settings.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/v0_9_2/src/u-googleanalytics.js"></script>');
 
-document.write('<script type="text/javascript" src="/js/lib/desktop/i-page.js"></script>');
-document.write('<script type="text/javascript" src="/js/lib/desktop/i-front.js"></script>');
-document.write('<script type="text/javascript" src="/js/lib/desktop/i-demo.js"></script>');
-document.write('<script type="text/javascript" src="/js/lib/desktop/i-login.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/desktop/m-page.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/desktop/m-front.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/desktop/m-demo.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/desktop/m-login.js"></script>');
 
--- www/js/lib/seg_smartphone_include.js
+++ www/js/lib/seg_smartphone_include.js
@@ -3,8 +3,8 @@ document.write('<script type="text/javascript" src="/js/manipulator/v0_9_2/merge
 document.write('<script type="text/javascript" src="/js/lib/u-settings.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/v0_9_2/src/u-googleanalytics.js"></script>');
 
-document.write('<script type="text/javascript" src="/js/lib/smartphone/i-page.js"></script>');
-document.write('<script type="text/javascript" src="/js/lib/smartphone/i-front.js"></script>');
-document.write('<script type="text/javascript" src="/js/lib/smartphone/i-demo.js"></script>');
-document.write('<script type="text/javascript" src="/js/lib/smartphone/i-login.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/smartphone/m-page.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/smartphone/m-front.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/smartphone/m-demo.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/smartphone/m-login.js"></script>');
 
